dc165ccee96acc8b9e817257bdd405382708b0e7
Rishikesh Yadav <yadavrishikesh53@gmail.com>
2022-05-10 17:07:57 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+// connect mongoose to mongodb
+mongoose.connect("mongodb://localhost:27017/hospitalDB");
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
+
+db.once("open", function () {
+    console.log("Connected to the Database :: MongoDB");
+});
+
+
+module.exports = db;
